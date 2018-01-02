5db46d8e72e5509550241acebe4f0dbba4ce0261
shelvadim <shelgachev@yahoo.com>
2018-01-01 20:59:18 -0800

    404 page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 //git remote set-url origin https://github.com/Shelvadim/React-Route-Template.git
@@ -29,15 +29,21 @@ const Page4=()=>(
     </div>
 );
 
+const NotFoundPage=()=>(
+    <div>
+       404! Page not found
+    </div>
+);
 
 const routes=(
     <BrowserRouter>
-    <div>
+    <Switch>
         <Route path="/" component={Page1} exact={true}/>
         <Route path="/page2" component={Page2}/>
         <Route path="/page3" component={Page3}/>
         <Route path="/page4" component={Page4}/>
-    </div>
+        <Route component={NotFoundPage}/>
+    </Switch>
     </BrowserRouter>
 );
 
