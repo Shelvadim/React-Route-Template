2859623fba4d08dea6dab9b5bafeb617ce6454fb
shelvadim <shelgachev@yahoo.com>
2018-01-02 20:29:33 -0800

    linking

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter,Route, Switch} from 'react-router-dom';
+import {BrowserRouter,Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 //git remote set-url origin https://github.com/Shelvadim/React-Route-Template.git
@@ -31,19 +31,32 @@ const Page4=()=>(
 
 const NotFoundPage=()=>(
     <div>
-       404! Page not found
+       404! <Link to="/">Go home</Link>
     </div>
+);
+
+const Header=()=>(
+    <header>
+       <h1>Header</h1>
+       <NavLink to="/" activeClassName="is-active" exact={true}>Page1</NavLink>
+       <NavLink to="/page2" activeClassName="is-active">Page2</NavLink>
+       <NavLink to="/page3" activeClassName="is-active">Page3</NavLink>
+       <NavLink to="/page4" activeClassName="is-active">Page4</NavLink>
+    </header>
 );
 
 const routes=(
     <BrowserRouter>
-    <Switch>
-        <Route path="/" component={Page1} exact={true}/>
-        <Route path="/page2" component={Page2}/>
-        <Route path="/page3" component={Page3}/>
-        <Route path="/page4" component={Page4}/>
-        <Route component={NotFoundPage}/>
-    </Switch>
+        <div>
+            <Header/>
+            <Switch>
+                <Route path="/" component={Page1} exact={true}/>
+                <Route path="/page2" component={Page2}/>
+                <Route path="/page3" component={Page3}/>
+                <Route path="/page4" component={Page4}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
+        </div>    
     </BrowserRouter>
 );
 
